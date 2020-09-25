f76c94b6e038d6866217e5209c0dfe873f28eaf7
waimgmg <mgwaimaungmaung@gmail.com>
2020-09-25 23:17:52 +0630

    Tuto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int changedDice = 1;
+  int changedDice2 = 1;
+
+  void changeDice() {
+    setState(() {
+      changedDice = Random().nextInt(6) + 1;
+      changedDice2 = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$changedDice2.png'),
+              onPressed: () {
+                changeDice();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changeDice();
+                },
+                child: Image.asset('images/dice$changedDice.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
